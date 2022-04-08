149cbb80c77723e77249c462e3d633a758e693dc
unknown <maximilian.gerhardt@rub.de>
2022-04-08 17:04:52 +0200

    Format

--- src/main.c
+++ src/main.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -36,69 +36,76 @@ PikaObj *pikaMain;
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 
-int fputc(int ch, FILE* f) {
-    HAL_UART_Transmit(&huart1, (uint8_t*)&ch, sizeof(ch), HAL_MAX_DELAY);
-    return ch;
+int fputc(int ch, FILE *f)
+{
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, sizeof(ch), HAL_MAX_DELAY);
+  return ch;
 }
 
-#include  <errno.h>
-#include  <sys/unistd.h> // STDOUT_FILENO, STDERR_FILENO
+#include <errno.h>
+#include <sys/unistd.h> // STDOUT_FILENO, STDERR_FILENO
 int _write(int file, char *data, int len)
 {
-   if ((file != STDOUT_FILENO) && (file != STDERR_FILENO))
-   {
-      errno = EBADF;
-      return -1;
-   }
+  if ((file != STDOUT_FILENO) && (file != STDERR_FILENO))
+  {
+    errno = EBADF;
+    return -1;
+  }
 
-   // arbitrary timeout 1000
-   HAL_StatusTypeDef status =
-      HAL_UART_Transmit(&huart1, (uint8_t*)data, len, 1000);
+  // arbitrary timeout 1000
+  HAL_StatusTypeDef status =
+      HAL_UART_Transmit(&huart1, (uint8_t *)data, len, 1000);
 
-   // return # of bytes written - as best we can tell
-   return (status == HAL_OK ? len : 0);
+  // return # of bytes written - as best we can tell
+  return (status == HAL_OK ? len : 0);
 }
 
-char __platform_getchar(void) {
-    uint8_t ch;
-    while(1){
-        if(HAL_OK == HAL_UART_Receive(&huart1, &ch, 1, 0)){
-            return ch;
-        }
+char __platform_getchar(void)
+{
+  uint8_t ch;
+  while (1)
+  {
+    if (HAL_OK == HAL_UART_Receive(&huart1, &ch, 1, 0))
+    {
+      return ch;
     }
+  }
 }
-
 
 void LED_off(PikaObj *self)
 {
-			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_SET);
 }
 void LED_on(PikaObj *self)
 {
-			HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET);
 }
 
 #include "BaseObj.h"
-void LED_offMethod(PikaObj *self, Args *args){
-    LED_off(self);
+void LED_offMethod(PikaObj *self, Args *args)
+{
+  LED_off(self);
 }
 
-void LED_onMethod(PikaObj *self, Args *args){
-    LED_on(self);
+void LED_onMethod(PikaObj *self, Args *args)
+{
+  LED_on(self);
 }
 
-PikaObj *New_LED(Args *args){
-    PikaObj *self = New_TinyObj(args);
-    class_defineMethod(self, "off()", LED_offMethod);
-    class_defineMethod(self, "on()", LED_onMethod);
-    return self;
+PikaObj *New_LED(Args *args)
+{
+  PikaObj *self = New_TinyObj(args);
+  class_defineMethod(self, "off()", LED_offMethod);
+  class_defineMethod(self, "on()", LED_onMethod);
+  return self;
 }
 
-PikaObj *New_MyRoot(Args *args){
-    PikaObj *self = New_BaseObj(args);
-    //obj_import(self, "LED", New_LED);
-    obj_newObj(self, "led", "LED", New_LED);
-    return self;
+PikaObj *New_MyRoot(Args *args)
+{
+  PikaObj *self = New_BaseObj(args);
+  // obj_import(self, "LED", New_LED);
+  obj_newObj(self, "led", "LED", New_LED);
+  return self;
 }
 
 int main(void)
@@ -127,12 +134,12 @@ int main(void)
   MX_USART1_UART_Init();
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
-  //pikaMain = pikaScriptInit();
-  PikaObj *root = newRootObj("root",New_MyRoot);
+  // pikaMain = pikaScriptInit();
+  PikaObj *root = newRootObj("root", New_MyRoot);
   printf("memory used now: %d B\r\n", pikaMemNow());
   printf("memory used max: %d B\r\n", pikaMemMax());
 
-  //pikaScriptShell(pikaScriptInit());
+  // pikaScriptShell(pikaScriptInit());
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -140,27 +147,27 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-		HAL_Delay(1000);
-		obj_run(root, "led.on()");
-		HAL_Delay(1000);
-		obj_run(root, "led.off()");
+    HAL_Delay(1000);
+    obj_run(root, "led.on()");
+    HAL_Delay(1000);
+    obj_run(root, "led.off()");
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -173,9 +180,8 @@ void SystemClock_Config(void)
     Error_Handler();
   }
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -212,12 +218,11 @@ static void MX_USART1_UART_Init(void)
   /* USER CODE BEGIN USART1_Init 2 */
 
   /* USER CODE END USART1_Init 2 */
-
 }
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -229,14 +234,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
