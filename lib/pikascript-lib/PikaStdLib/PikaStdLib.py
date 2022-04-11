ec8bf875fe8be8feabe50407714133e4721fe2f2
unknown <maximilian.gerhardt@rub.de>
2022-04-11 21:30:34 +0200

    Update PikaCore + libs

--- lib/pikascript-lib/PikaStdLib/PikaStdLib.py
+++ lib/pikascript-lib/PikaStdLib/PikaStdLib.py
@@ -1,19 +1,63 @@
 from PikaObj import *
 
-class MemChecker(BaseObj):
-    def max():
+
+class MemChecker(TinyObj):
+    def max(self):
         pass
 
-    def now():
+    def now(self):
         pass
 
-    def resetMax():
+    def getMax(self) -> float:
+        pass
+
+    def getNow(self) -> float:
+        pass
+
+    def resetMax(self):
         pass
 
 
 class SysObj(BaseObj):
-    def type(argPath: str):
+    def type(self, arg: any):
         pass
 
-    def remove(argPath: str):
+    def remove(self, argPath: str):
         pass
+
+    def int(self, arg: any) -> int:
+        pass
+
+    def float(self, arg: any) -> float:
+        pass
+
+    def str(self, arg: any) -> str:
+        pass
+
+    def iter(self, arg: any) -> any:
+        pass
+
+    def range(self, a1: int, a2: int) -> any:
+        pass
+
+    def __set__(self, obj: any, key: any, val: any, obj_str: str):
+        pass
+
+    def __get__(self, obj: any, key: any) -> any:
+        pass
+
+    def len(self, arg:any) -> int:
+        pass
+
+
+class RangeObj(TinyObj):
+    def __next__(self) -> any:
+        pass
+
+
+class StringObj(TinyObj):
+    def __next__(self) -> any:
+        pass
+
+class PikaObj(TinyObj):
+    pass
